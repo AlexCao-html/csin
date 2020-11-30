f16348b6a6ca09f4f02f2208338a6a2855d4e21c
AlexCao-html <caobubo@gmail.com>
2020-11-30 15:02:30 -0800

    made the how to images

--- frontend/script.js
+++
@@ -1,4 +0,0 @@
-import Banner from "./components/header.vue"
-import Navagation from "./components/navagation.vue"
-import Main from "./components/main.vue"
-import Footer from "./components/footer.vue"
